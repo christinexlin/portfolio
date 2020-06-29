c504edea7100b61f83e4943e64f144f5ba0ba05d
Christine Lin <chin_hsuen_lin@brown.edu>
2020-06-29 23:02:00 +0800

    Updates

---
+++ precache-manifest.1269ab6631a87887169f2f23a42cf5ef.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457a64e2e0a8f12a74647ca02504b72a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f343063bf91e041141bb",
+    "url": "/static/css/main.4659a351.chunk.css"
+  },
+  {
+    "revision": "7564c77909aa6a78d47d",
+    "url": "/static/js/2.c80a0c9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c80a0c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f343063bf91e041141bb",
+    "url": "/static/js/main.15abf02d.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "334c139f6cb7b809bbfa996c6679ca2b",
+    "url": "/static/media/1.334c139f.png"
+  },
+  {
+    "revision": "e7f45eb2db25f18c42a094b6045e7634",
+    "url": "/static/media/10.e7f45eb2.png"
+  },
+  {
+    "revision": "ef4f993c51b47616d67a61e25c003f19",
+    "url": "/static/media/11.ef4f993c.png"
+  },
+  {
+    "revision": "a5aeec60714c1834761f410ec99a2949",
+    "url": "/static/media/12.a5aeec60.png"
+  },
+  {
+    "revision": "542a1571bf689777a0894297fe46ba7b",
+    "url": "/static/media/13.542a1571.png"
+  },
+  {
+    "revision": "6862af5db59e35bb35671bec65e1f2e0",
+    "url": "/static/media/14.6862af5d.png"
+  },
+  {
+    "revision": "150efad5e61540b1aeedc5995f4fd447",
+    "url": "/static/media/15.150efad5.png"
+  },
+  {
+    "revision": "27ac15ba371ca3a1c4f29750d8d00a7d",
+    "url": "/static/media/16.27ac15ba.png"
+  },
+  {
+    "revision": "30bcb8d25dcf5a017748970081fc8707",
+    "url": "/static/media/17.30bcb8d2.png"
+  },
+  {
+    "revision": "8f2f9ea75601a1c76bc402fc3b067b6e",
+    "url": "/static/media/18.8f2f9ea7.png"
+  },
+  {
+    "revision": "837f4a1d43b81b7b7cf48f4e05e2119a",
+    "url": "/static/media/19.837f4a1d.png"
+  },
+  {
+    "revision": "0fec03cc4bb666baa9f027b2e8113b88",
+    "url": "/static/media/2.0fec03cc.png"
+  },
+  {
+    "revision": "5668c745c93ded45c282745b471abb4d",
+    "url": "/static/media/20.5668c745.png"
+  },
+  {
+    "revision": "7765e5717ca72cccd2f2ffe0a839083a",
+    "url": "/static/media/21.7765e571.png"
+  },
+  {
+    "revision": "184c33cefc124742741b84440cc4e369",
+    "url": "/static/media/25.184c33ce.png"
+  },
+  {
+    "revision": "6ea6e04882deaab4e1e4045cf1664661",
+    "url": "/static/media/3.6ea6e048.png"
+  },
+  {
+    "revision": "083b8e8f5b347d8285d2dfb5516cbd88",
+    "url": "/static/media/4.083b8e8f.png"
+  },
+  {
+    "revision": "da39fe2e228f9a25de5b8b69b8df8fad",
+    "url": "/static/media/4.da39fe2e.png"
+  },
+  {
+    "revision": "094b16a9ea339e314d557405169ced8a",
+    "url": "/static/media/5.094b16a9.png"
+  },
+  {
+    "revision": "399de345b950191c714938356f578bd0",
+    "url": "/static/media/6.399de345.png"
+  },
+  {
+    "revision": "592fd2e4949adb156518515264e77bd5",
+    "url": "/static/media/6.592fd2e4.png"
+  },
+  {
+    "revision": "dc75965fe915941e6e36e8b942b7c731",
+    "url": "/static/media/7.dc75965f.png"
+  },
+  {
+    "revision": "f5f83cb52c28d9129f98374256186bc6",
+    "url": "/static/media/8.f5f83cb5.png"
+  },
+  {
+    "revision": "94cd83e0dfd9e3cc1862634ac9bdb224",
+    "url": "/static/media/9.94cd83e0.png"
+  },
+  {
+    "revision": "61248913e379ead7dff4aff3299facba",
+    "url": "/static/media/bluenotes.61248913.png"
+  },
+  {
+    "revision": "8ec8357ed654bcff61edbcdf55a05b32",
+    "url": "/static/media/christine.8ec8357e.jpg"
+  },
+  {
+    "revision": "49fbdbe26127b4ce6cd7884d4e164475",
+    "url": "/static/media/critterpedia.49fbdbe2.png"
+  },
+  {
+    "revision": "bd02e7bc58b84b9327293343e9bb51b8",
+    "url": "/static/media/filterui.bd02e7bc.png"
+  },
+  {
+    "revision": "36c5740f84ccdd01f7c95262f0a886f8",
+    "url": "/static/media/fullpage.36c5740f.png"
+  },
+  {
+    "revision": "f47d3dfea140213247d1af5700c10f05",
+    "url": "/static/media/illustrations.f47d3dfe.png"
+  },
+  {
+    "revision": "c4bfac762ab713aa3952f98b89c2e9c5",
+    "url": "/static/media/moodboard.c4bfac76.png"
+  },
+  {
+    "revision": "cf0afddbcda2032980b409d8cda8a723",
+    "url": "/static/media/screenshot-2.cf0afddb.png"
+  },
+  {
+    "revision": "72e23a8c56d25dd3db1f8da4dc7f2a35",
+    "url": "/static/media/screenshot.72e23a8c.png"
+  },
+  {
+    "revision": "c05c33849e2ab26fcb209dfe1dbec9ac",
+    "url": "/static/media/signofchange.c05c3384.gif"
+  },
+  {
+    "revision": "278e0d8b5fc7f74a2882452b9b93f1a0",
+    "url": "/static/media/tomnook.278e0d8b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b6228e618cb04a7b073be619b484d3c6.js"
+  "/precache-manifest.1269ab6631a87887169f2f23a42cf5ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
