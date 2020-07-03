b7f2310ae762b3fff9da355c9c647d552775a922
Christine Lin <chin_hsuen_lin@brown.edu>
2020-07-03 18:51:03 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5c31810f9e19c8a1a85be0663c39f3c.js"
+  "/precache-manifest.ebeff38e0c98b6b05a772f38476fd73b.js"
 );
 
 self.addEventListener('message', (event) => {
