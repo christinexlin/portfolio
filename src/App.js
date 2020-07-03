02b153c4b86a1ecfe668eb125fd042ecbf7b7301
Christine Lin <chin_hsuen_lin@brown.edu>
2020-07-03 18:46:29 +0800

    update landing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { HashRouter, BrowserRouter, Route, NavLink, useLocation } from 'react-router-dom';
 import './App.css';
 import Projects from './Projects.js';
@@ -9,42 +9,46 @@ import Bluenotes from './Projects/Bluenotes.js';
 import Experiments from './Experiments/Experiments.js';
 import ScrollToTop from './ScrollToTop.js';
 
-function App() {
-  return (
-    <BrowserRouter basename="/">
-    <ScrollToTop />
-        <div className="App">
-            <div className='navigation'>
-                <div className="navigation-sub links">
-                    <NavLink basename={process.env.PUBLIC_URL} exact to="/" className="link" activeClassName="selected">PROJECTS</NavLink>
-                    <NavLink  basename={process.env.PUBLIC_URL} to="/experiments" className="link" activeClassName="selected">EXPERIMENTS</NavLink>
-                    <NavLink basename={process.env.PUBLIC_URL} exact to="/about" className="link" activeClassName="selected">ABOUT</NavLink>
-                </div>
-            </div>
+class App extends Component {
 
-            <Route exact basename={process.env.PUBLIC_URL} path="/" component={Projects} />
-            <Route basename={process.env.PUBLIC_URL} path="/experiments" component={Experiments} />
-            <Route basename={process.env.PUBLIC_URL} path="/about" component={About} />
-            <Route basename={process.env.PUBLIC_URL} path="/critterpedia" component={Critterpedia} />
-            <Route basename={process.env.PUBLIC_URL} path="/bluenotes" component={Bluenotes} />
+    render() {
+        return (
+          <BrowserRouter basename="/">
+          <ScrollToTop />
+              <div className="App">
+                  <div className='navigation'>
+                      <div className="navigation-sub links">
+                          <NavLink basename={process.env.PUBLIC_URL} exact to="/" className="link" activeClassName="selected">PROJECTS</NavLink>
+                          <NavLink  basename={process.env.PUBLIC_URL} to="/experiments" className="link" activeClassName="selected">EXPERIMENTS</NavLink>
+                          <NavLink basename={process.env.PUBLIC_URL} exact to="/about" className="link" activeClassName="selected">ABOUT</NavLink>
+                      </div>
+                  </div>
+
+                  <Route exact basename={process.env.PUBLIC_URL} path="/" component={Projects} />
+                  <Route basename={process.env.PUBLIC_URL} path="/experiments" component={Experiments} />
+                  <Route basename={process.env.PUBLIC_URL} path="/about" component={About} />
+                  <Route basename={process.env.PUBLIC_URL} path="/critterpedia" component={Critterpedia} />
+                  <Route basename={process.env.PUBLIC_URL} path="/bluenotes" component={Bluenotes} />
 
 
-        </div>
-        <div className="footer container-fluid">
-        <div className="row">
-        <div>
-        <p>LINKS AND THINGS <img className="tamagotchi" src={tamagotchi}/></p>
-        </div>
-<div>
-            <a className="link" href="mailto: chin_hsuen_lin@brown.edu">EMAIL</a>
-            <a className="link" href="https://github.com/christinexlin">GITHUB</a>
-            <a className="link" href="https://www.linkedin.com/in/christine-lin-01/">LINKEDIN</a>
-            <a className="link" href="https://twitter.com/christinexlin">TWITTER</a>
-        </div>
-            </div>
-        </div>
-    </BrowserRouter>
-  );
+              </div>
+              <div className="footer container-fluid">
+              <div className="row">
+              <div>
+              <p>LINKS AND THINGS <img className="tamagotchi" src={tamagotchi}/></p>
+              </div>
+      <div>
+                  <a className="link" href="mailto: chin_hsuen_lin@brown.edu">EMAIL</a>
+                  <a className="link" href="https://github.com/christinexlin">GITHUB</a>
+                  <a className="link" href="https://www.linkedin.com/in/christine-lin-01/">LINKEDIN</a>
+                  <a className="link" href="https://twitter.com/christinexlin">TWITTER</a>
+              </div>
+                  </div>
+              </div>
+          </BrowserRouter>
+        );
+
+    }
 }
 
 export default App;
--- src/Projects.js
+++ src/Projects.js
@@ -1,51 +1,102 @@
-import React from "react";
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, {Component} from 'react';
 import Thumbnail from './Thumbnail.js';
 import critterpedia from './Projects/critterpedia.png';
 import { Twemoji } from 'react-emoji-render';
 import tamagotchi2 from './tamagotchi2.png';
 import tom from './Projects/tomnook.png';
 import bluenotes from './Projects/bluenotes.png';
+import fullpage from './Projects/Critterpedia/fullpage.png';
+import zanyface from './zany-face_1f92a.png';
+import p5 from 'p5';
+import Sketch from 'p5';
 
 
-function Projects(props) {
+class Projects extends Component {
 
-  return (
-<div>
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
 
-        <div className="intro">
-            <div className="intro-sub">
-                <h1>Christine Lin</h1>
-                <h2>Hello!
-                I'm a product designer and developer based in Taipei.
-                I currently study Computer Science at Brown University.
-                <img className="tamagotchi" src={tamagotchi2}/>
-                </h2>
+    sketch = (s) => {
+        let x = 0;
+        let y = 0;
+        let emojis = ["🤪","😄"];
+        let currIndex = Math.floor(Math.random() * emojis.length);
+        let currEmoji = emojis[currIndex];
 
+      s.setup = () => {
+        s.createCanvas(s.windowWidth, s.windowHeight);
+        s.noStroke();
+      }
+
+      s.mousePressed = () => {
+          let newIndex;
+          do {
+              newIndex = Math.floor(Math.random() * emojis.length);
+          }
+          while (newIndex == currIndex);
+          currIndex = newIndex;
+          currEmoji = emojis[currIndex];
+      }
+
+      s.draw = () => {
+        s.background(255);
+        x = s.lerp(x, s.mouseX, 0.05);
+        y = s.lerp(y, s.mouseY, 0.05);
+
+        s.fill(0);
+        s.stroke(0);
+        s.textSize(50);
+        s.text(currEmoji, x, y);
+      }
+
+    }
+
+    componentDidMount() {
+    this.myP5 = new p5(this.sketch, this.myRef.current);
+    }
+
+  render() {
+      return (
+          <div>
+          <div className="p5" ref={this.myRef}>
+          </div>
+            <div className="intro">
+
+                <div className="intro-sub">
+                    <h1>Christine Lin</h1>
+                    <h2>Hello!
+                    I'm a product designer and developer based in Taipei.
+                    I currently study Computer Science at Brown University.
+                    </h2>
+
+                </div>
             </div>
-        </div>
 
-        <div className="projects">
-        <Thumbnail
-            link="/critterpedia"
-            image={critterpedia}
-            title="Blather's Critterpedia"
-            category={['FRONT-END DEVELOPMENT', 'WEB DESIGN']}
-            description="React search + filter site to help players discover bugs and fish in Animal Crossing"
-            icon={tom}
-        />
-        <Thumbnail
-            link="/bluenotes"
-            image={bluenotes}
-            title="Bluenotes"
-            category={['CASE STUDY', 'PRODUCT DESIGN']}
-            description="Q&A forum to support new students at Brown as they adjust to campus life"
-        />
-        </div>
+            <div className="projects">
+            <Thumbnail
+                link="/critterpedia"
+                image={critterpedia}
+                title="Blather's Critterpedia"
+                category={['FRONT-END DEVELOPMENT', 'WEB DESIGN']}
+                description="React search + filter site to help players discover bugs and fish in Animal Crossing"
+                icon={tom}
+            />
+            <Thumbnail
+                link="/bluenotes"
+                image={bluenotes}
+                title="Bluenotes"
+                category={['CASE STUDY', 'PRODUCT DESIGN']}
+                description="Q&A forum to support new students at Brown as they adjust to campus life"
+            />
+            </div>
 
 
-</div>
-  )
+    </div>
+);
+
+  }
 }
 
 export default Projects;
